c5a7d2f4eca0f37e68d0c7a45417661e3c1b88fe
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-27 18:22:08 -0300

    Criando componente de navigation bar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { NavComponent } from './componentes/nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
