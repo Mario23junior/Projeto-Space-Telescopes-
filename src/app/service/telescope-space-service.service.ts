5f4a97e391eeec2df3e320a65a3af9e5f280263e
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-29 19:17:35 -0300

    Criando camada de serviço do modelo de dados telescope

---
+++ src/app/service/telescope-space-service.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
+import { TelescopeSpace } from '../model/TelescopeSpace.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TelescopeSpaceServiceService {
+
+  private baseUrl = "http://localhost:8080"
+  private url = "/v1/api/base/telescope/";
+
+  constructor(private httpClien: HttpClient) { }
+
+  listAllTelescope() {
+    return this.httpClien.get<TelescopeSpace[]>(this.baseUrl + this.url)
+      .pipe(
+        first(),
+        delay(1200),
+        tap(telescope => console.log(telescope))
+      );
+  }
+}
