66be9a48d0cc0a29652e4099c6e0b1aa034b93e9
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-01 03:07:12 -0300

    Criando formulario de inserção de dados

--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -13,7 +13,7 @@ export class NavComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.router.navigate(['telescope'])
+    this.router.navigate(['createTelescope'])
   }
 
 }
--- src/app/componentes/telescope/telescopios-create/telescopios-create.component.ts
+++ src/app/componentes/telescope/telescopios-create/telescopios-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { TelescopeSpaceServiceService } from 'src/app/service/telescope-space-service.service';
 
 @Component({
   selector: 'app-telescopios-create',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelescopiosCreateComponent implements OnInit {
 
-  constructor() { }
+  public formBuilder: FormBuilder
+  public form: FormGroup
+  public service: TelescopeSpaceServiceService
+
+
+  constructor(
+    formBuilder: FormBuilder,
+    service: TelescopeSpaceServiceService,
+    form:FormGroup
+  ) {
+    this.formBuilder = formBuilder
+    this.service = service
+    this.form = this.formBuilder.group({
+        nome:[null],
+        img:[null],
+        tipo:[null],
+        missao:[null],
+        durationMissao:[null],
+        dataDelancamento:[null],
+        status:[null]
+
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  public onSubmit(){
+
   }
 
 }
