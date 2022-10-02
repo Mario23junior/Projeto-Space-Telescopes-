488687797adaf8e9db00c36d2a46e3843455ede3
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-01 23:05:18 -0300

    Criando display de exibição de colunas

--- src/app/componentes/telescope/telescope-listcrud/telescope-listcrud.component.ts
+++ src/app/componentes/telescope/telescope-listcrud/telescope-listcrud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TelescopeSpace } from 'src/app/model/TelescopeSpace.model';
 
 @Component({
   selector: 'app-telescope-listcrud',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./telescope-listcrud.component.css']
 })
 export class TelescopeListcrudComponent implements OnInit {
+
+  telescope:TelescopeSpace[] = []
+  displayColumns = ['nome','tipo','Missão','Duração da Missão','Data de lançamento','status']
 
   constructor() { }
 
